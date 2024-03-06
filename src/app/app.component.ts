6fcf77a244e56a26b263f3a89eb3f52c1b3bb5b0
daisygith <daisygith@users.noreply.github.com>
2024-03-06 10:00:44 +0100

    created role and used [ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RoomsComponent} from "./rooms/rooms.component";
+import {NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RoomsComponent],
+  imports: [RouterOutlet, RoomsComponent, NgSwitchCase, NgSwitch, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  role = 'Admin';
 }
